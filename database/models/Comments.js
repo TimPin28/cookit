25d6622ed400952d86ce42aeb53c7f73839a2dc9
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-07-01 23:16:20 +0800

    added editable boolean property

--- database/models/Comments.js
+++ database/models/Comments.js
@@ -4,6 +4,7 @@ const CommentSchema = new mongoose.Schema({
     author:         {type: String, required: true},
     ogPost:         {type: String, required: true},
     comment_text:   {type: String, required: true},
+    editable:       {type: Boolean, default: false}
 },{timestamps: true});
 
 const Comments = mongoose.model('Comments', CommentSchema);
