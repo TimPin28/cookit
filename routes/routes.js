cb1a49bc2a2c7d0bb1ee43ef2db10f29a9702b0b
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-07-01 09:57:41 +0800

    added isPrivate requirement for /comment-post and /settings

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ router.get('/', controller.getIndex);
 router.get('/alpbt', controller.getAlpbt);
 
 router.get('/create', isPrivate, controller.getCreate);
-router.get('/settings', controller.getSettings);
+router.get('/settings', isPrivate, controller.getSettings);
 router.get('/login', isPublic, controller.getLogin);
 router.get('/register', isPublic, controller.getRegister);
 router.get('/profile', controller.getProfile);
@@ -18,7 +18,7 @@ router.get('/profile', controller.getProfile);
 router.post('/register', isPublic, registerValidation, controller.registerUser);
 router.post('/login', isPublic, loginValidation, controller.loginUser);
 router.post('/submit-post', isPrivate, controller.submitPost);
-router.post('/comment-post', controller.commentPost);
+router.post('/comment-post', isPrivate, controller.commentPost);
 router.post('/search-post', controller.searchPost);
 
 router.get('/logout', isPrivate, controller.logoutUser);
