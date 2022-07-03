9d6b1c0abc65e1d81692039362356904b47fbd39
TimPin28 <timothy_vincent_pinawin@dlsu.edu.ph>
2022-07-03 15:08:09 +0800

    test

--- controllers/controller.js
+++ controllers/controller.js
@@ -357,6 +357,8 @@ const controller = {
         var postID = req.get('referer');
         postID = postID.replace("https://cookit-apdev.herokuapp.com/viewPost?valid=", "");
         Post.findOne({_id: new Object(postID)}, (error, post) => {
+            const test = post;
+            console.log(test);
             if (req.session.username) {
                 res.render('createedit', {title: post.title, tags: post.tags, ingredients: post.ingredients, instructions: post.instructions,
                      loggedin: true, loggeduser: req.session.username});
